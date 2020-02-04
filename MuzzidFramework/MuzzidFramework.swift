01595e8fb97011eb9cfa9970a7747b7a74813681
taind <nguyenductai.job@gmail.com>
2020-02-04 16:53:26 +0700

    0.0.2

---
+++ MuzzidFramework/MuzzidFramework.swift
@@ -0,0 +1,25 @@
+//
+//  MuzzidFramework.swift
+//  MuzzidFramework
+//
+//  Created by Tai Nguyen on 2/4/20.
+//  Copyright © 2020 Tai Nguyen. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+public class MuzzidFramework {
+
+    private init() {}
+    
+    public static func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public static func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+   
+    
+}
